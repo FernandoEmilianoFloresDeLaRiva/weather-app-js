1f2a63f36eb45af954a3f45b29cb49bb6ddd2309
Fernando Flores <137421927+FernandoEmilianoFloresDeLaRiva@users.noreply.github.com>
2023-09-29 08:37:24 -0600

    Update weatherApi.js

--- src/services/api/weatherApi.js
+++ src/services/api/weatherApi.js
@@ -1,4 +1,4 @@
-const api_key = "{YOUR API KEY HERE}";
+const api_key = "81ad4d60fa7791c8de0427eb55951f0f";
 const api_url = `https://api.openweathermap.org/data/2.5/weather?appid=${api_key}&units=metric`;
 
 export const getWeather = async (city) => {
